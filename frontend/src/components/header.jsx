b9c64a14e06d6c62670ea34333236448c2ad9c10
CoderTofu <vioneverforget@gmail.com>
2024-08-08 20:39:45 +0800

    optimized directory

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/header.css";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>AI Assistant</h1>
+    </div>
+  );
+}
---
+++ frontend/src/components/signup.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "../css/signup.css";
+
+export default function SignUp() {
+  return (
+    <div>
+      <h1>AI Assistant</h1>
+    </div>
+  );
+}
